eda181b052e3e80f6b06341f75d702b2ca7e3550
Iwan Budihalim <iwan_budihalim@visuallabs.net>
2018-11-20 23:00:30 +0700

    first commit

--- app.go
+++ app.go
@@ -4,15 +4,13 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-	"github.com/sbourne20/examgo3/models"
+	"github.com/sbourne20/examgo5/controller"
 )
 
 type App struct {
@@ -21,10 +19,11 @@ type App struct {
 }
 
 func (a *App) Initialize(user, password, dbname string) {
-	connectionString := fmt.Sprintf("%s:%s@tcp(rm-d9jxms81910c50lvi.mysql.ap-southeast-5.rds.aliyuncs.com:3306)/%s?charset=utf8&parseTime=True", user, password, dbname)
-	//connectionString := fmt.Sprintf("%s:%s@tcp(192.168.8.32:3306)/%s?charset=utf8&parseTime=True", user, password, dbname)
+	//connectionString := fmt.Sprintf("%s:%s@tcp(rm-d9jxms81910c50lvi.mysql.ap-southeast-5.rds.aliyuncs.com:3306)/%s?charset=utf8&parseTime=True", user, password, dbname)
+	connectionString := fmt.Sprintf("%s:%s@tcp(192.168.8.32:3306)/%s?charset=utf8&parseTime=True", user, password, dbname)
 
 	var err error
+
 	a.DB, err = sql.Open("mysql", connectionString)
 	if err != nil {
 		log.Fatal(err)
@@ -39,106 +38,27 @@ func (a *App) Run(addr string) {
 }
 
 func (a *App) initializeRoutes() {
-	a.Router.HandleFunc("/testAgent", a.testAgents).Methods("GET")
-	a.Router.HandleFunc("/getNews/{id}", a.getNews).Methods("GET")
+
+	a.Router.HandleFunc("/testAgent", controller.TestAgents).Methods("GET")
 	a.Router.HandleFunc("/users", a.getUsers).Methods("GET")
 	a.Router.HandleFunc("/user", a.createUser).Methods("POST")
 	a.Router.HandleFunc("/user/{id}", a.updateUser).Methods("PUT")
 	a.Router.HandleFunc("/user/{id}", a.deleteUser).Methods("DELETE")
-}
-
-func (a *App) testAgents(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
-func (a *App) getNews(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	var newsx = models.RetrieveNews(vars["id"])
-	fmt.Fprintf(w, newsx)
+	a.Router.HandleFunc("/getNews/{id}", controller.GetNews).Methods("GET")
 }
 
 func (a *App) getUsers(w http.ResponseWriter, r *http.Request) {
-
-	//user, err := users.GetUsers(a.DB)
-	models, err := models.GetUsers(a.DB)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, models)
+	controller.GetUsers(w, r, a.DB)
 }
 
 func (a *App) createUser(w http.ResponseWriter, r *http.Request) {
-	//var u users.User
-	var u models.User
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&u); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
-	defer r.Body.Close()
-
-	if err := u.CreateUser(a.DB); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(w, http.StatusCreated, u)
+	controller.CreateUser(w, r, a.DB)
 }
 
 func (a *App) updateUser(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
-		return
-	}
-
-	//var u users.User
-	var u models.User
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&u); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid resquest payload")
-		return
-	}
-	defer r.Body.Close()
-	u.Idpengguna = id
-
-	if err := u.UpdateUser(a.DB); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, u)
+	controller.UpdateUser(w, r, a.DB)
 }
 
 func (a *App) deleteUser(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid User ID")
-		return
-	}
-
-	//u := users.User{Idpengguna: id}
-	u := models.User{Idpengguna: id}
-	if err := u.DeleteUser(a.DB); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
-}
-
-func respondWithError(w http.ResponseWriter, code int, message string) {
-	respondWithJSON(w, code, map[string]string{"error": message})
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
+	controller.DeleteUser(w, r, a.DB)
 }
--- main.go
+++ main.go
@@ -12,10 +12,7 @@ package main
 
 func main() {
 	a := App{}
-
-	// You need to set your Username and Password here
-	a.Initialize("root", "Master199", "bkd")
-	//a.Initialize("root", "@Bourne20", "bkd")
-
+	a.Initialize("root", "@Bourne20", "bkd")
+	//a.Initialize("root", "Master199", "bkd")
 	a.Run(":8080")
 }
